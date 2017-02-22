3eb644ccdcc0c073993e555449655f1a7ca78b74
Silence <taogu2010@yeah.net>
2017-02-22 19:23:56 +0800

    更新SQL文件

--- eclipse/silence/src/main/sql/schema.sql
+++ eclipse/silence/src/main/sql/schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE history (
   history_id BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '项目进程id',
   title VARCHAR(50) DEFAULT NULL COMMENT '标题',
   content VARCHAR(200) NOT NULL COMMENT '此次进程内容',
-  picture_path VARCHAR(100) DEFAULT NULL COMMENT '附加图片路径',
+  history_sign VARCHAR(100) DEFAULT NULL COMMENT '唯一标识sign',
   record_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '记录时间',
   PRIMARY KEY (history_id),
   KEY idx_record_time (record_time)
